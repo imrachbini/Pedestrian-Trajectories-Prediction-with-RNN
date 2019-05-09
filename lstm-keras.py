7ed556704dd826e531419e7c9ae47966775100bf
Imam M. Rachbini <imrachbini@gmail.com>
2019-05-09 15:43:41 +0700

    run on multi gpu

--- lstm-keras.py
+++ lstm-keras.py
@@ -10,26 +10,32 @@ from keras.layers import Input
 from keras.layers import TimeDistributed
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.utils.training_utils import multi_gpu_model
 
-
+load_weight = False
 num_feature = 5
-batch_size = 30
-rnn_size = 400
+batch_size = 4
+rnn_size = 512
 output_size = 1
 learning_rate = 0.0005
 
 training_X, training_Y, dev_X, dev_Y, testing_X, testing_Y = read_data.aline_data('data_train.csv', num_feature)
 
-model = Sequential()
-model.add(
-    LSTM(rnn_size, 
-         input_shape=(training_X.shape[1], training_X.shape[2])
+with tf.device("/cpu:0"):
+    model = Sequential()
+    model.add(
+        LSTM(rnn_size, 
+            input_shape=(training_X.shape[1], training_X.shape[2])
+        )
     )
-)
-model.add(Dense(2))
-model.compile(loss='mse', optimizer='adam')
+    model.add(Dense(2))
+    if load_weight:
+        model.load_weight('misc/keras_lstm.h5')
 
-history = model.fit(training_X, training_Y, epochs=100, batch_size=num_feature,
-                    validation_data=(dev_X, dev_Y), verbose=2, shuffle=False)
+gpu_model = multi_gpu_model(model, gpus=2)
+gpu_model.compile(loss='mse', optimizer='adam')
+
+history = gpu_model.fit(training_X, training_Y, epochs=100, batch_size=batch_size,
+                    validation_data=(dev_X, dev_Y), verbose=1, shuffle=False)
 
 model.save('misc/keras_lstm.h5')
